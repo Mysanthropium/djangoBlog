ea716f0f156447cc7615458b8b569a1d2d89e850
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-06-13 08:58:26 +0000

    second deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mysanthropiu-djangoblog-ee3mo8y88fu.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['rasmus-djangoblog.herokuapp.com', 'localhost']
 
 
 # Application definition
